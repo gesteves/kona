0d5ecc02b86c4450a4a1f575d7aee15d5a91344b
Guillermo Esteves <g@gesteves.com>
2023-08-07 07:50:08 -0600

    Don't query for dropcaps

--- lib/import/contentful.rb
+++ lib/import/contentful.rb
@@ -27,7 +27,6 @@ module Import
             }
             summary
             published
-            showDropCap
             indexInSearchEngines
             canonicalUrl
             openGraphImage {
