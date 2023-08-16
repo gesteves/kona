12fe85ee28d7b4c1cabb68f7f1cdeca0613f9d65
Guillermo Esteves <g@gesteves.com>
2023-08-16 10:28:47 -0600

    Tweak precision

--- helpers/custom_helpers.rb
+++ helpers/custom_helpers.rb
@@ -365,7 +365,13 @@ module CustomHelpers
   end
 
   def formatted_distance(meters)
-    precision = meters >= 10000 ? 1 : 2
+    precision = if meters < 10000
+      2
+    elsif meters < 100000
+      1
+    else
+      0
+    end
     number_to_human(meters, units: DISTANCE_UNITS, precision: precision, strip_insignificant_zeros: true, significant: false, delimiter: ',')
   end
 
