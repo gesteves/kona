403ce6bceb1ea018943767c5c4836c1b34abda1e
Guillermo Esteves <g@gesteves.com>
2023-08-19 14:57:33 -0600

    Add cron schedule directly

--- netlify/functions/scheduled-deploy.js
+++ netlify/functions/scheduled-deploy.js
@@ -1,25 +1,14 @@
 import fetch from 'node-fetch'
 import { schedule } from '@netlify/functions'
-import isValidCron from 'cron-validator'
 
-// Access environment variables
 const BUILD_HOOK = process.env.BUILD_HOOK_URL
-const CRON_SCHEDULE = process.env.BUILD_CRON_SCHEDULE
 
-// Check if the necessary environment variables are set
-if (!BUILD_HOOK || !CRON_SCHEDULE) {
-  console.error('Error: The BUILD_HOOK_URL and/or BUILD_CRON_SCHEDULE environment variables are not set.')
+if (!BUILD_HOOK) {
+  console.error('Error: The BUILD_HOOK_URL environment variable is not set.')
   process.exit(1) // Exit with a failure code
 }
 
-// Validate the cron schedule
-if (!isValidCron(CRON_SCHEDULE)) {
-  console.error('Error: The CRON_SCHEDULE environment variable is not a valid cron expression.')
-  process.exit(1) // Exit with a failure code
-}
-
-// Schedules the handler function to run based on the CRON_SCHEDULE environment variable
-const handler = schedule(CRON_SCHEDULE, async () => {
+const handler = schedule('0 12-23 * * *', async () => {
   await fetch(BUILD_HOOK, {
     method: 'POST'
   }).then(response => {
