c4309aee62ae3caaac7b45fec5a4c26574eadef5
Guillermo Esteves <g@gesteves.com>
2023-08-19 15:09:39 -0600

    Only call hook between 6-9 PM

--- netlify/functions/scheduled-deploy.js
+++ netlify/functions/scheduled-deploy.js
@@ -3,19 +3,20 @@ import { schedule } from '@netlify/functions'
 
 const BUILD_HOOK = process.env.BUILD_HOOK_URL
 
-if (!BUILD_HOOK) {
-  console.error('Error: The BUILD_HOOK_URL environment variable is not set.')
-  process.exit(1) // Exit with a failure code
-}
+const handler = schedule('0 * * * *', async () => {
+  // Convert current UTC hour to MDT (UTC-6)
+  const currentHourMDT = (new Date().getUTCHours() - 6 + 24) % 24;
 
-const handler = schedule('0 12-23 * * *', async () => {
-  await fetch(BUILD_HOOK, {
-    method: 'POST'
-  }).then(response => {
-    console.log('Build hook response:', response)
-  }).catch(error => {
-    console.error('Error in fetching build hook:', error)
-  })
+  // Check if current MDT hour is between 6 AM and 9 PM
+  if ((currentHourMDT >= 6 && currentHourMDT <= 21) && (BUILD_HOOK)) {
+    await fetch(BUILD_HOOK, {
+      method: 'POST'
+    }).then(response => {
+      console.log(`Build hook response status: ${response.status} ${response.statusText}`);
+    }).catch(error => {
+      console.error('Error in fetching build hook:', error)
+    })
+  }
 
   return {
     statusCode: 200
