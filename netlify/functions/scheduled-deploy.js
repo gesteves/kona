3c5f49b959d5d4bef1b68d817b5d0675104d0b70
Guillermo Esteves <g@gesteves.com>
2023-08-19 15:26:45 -0600

    Only run from 8 am to 8 pm

--- netlify/functions/scheduled-deploy.js
+++ netlify/functions/scheduled-deploy.js
@@ -7,8 +7,8 @@ const handler = schedule('0 * * * *', async () => {
   // Convert current UTC hour to MDT (UTC-6)
   const currentHourMDT = (new Date().getUTCHours() - 6 + 24) % 24;
 
-  // Check if current MDT hour is between 6 AM and 9 PM
-  if ((currentHourMDT >= 6 && currentHourMDT <= 21) && (BUILD_HOOK)) {
+  // Check if current MDT hour is between 8 AM and 8 PM
+  if (currentHourMDT >= 8 && currentHourMDT <= 20) && (BUILD_HOOK)) {
     await fetch(BUILD_HOOK, {
       method: 'POST'
     }).then(response => {
