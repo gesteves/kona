de706beff24e8097a4b00f21a2edd640529ab5ee
Guillermo Esteves <g@gesteves.com>
2023-08-20 05:20:23 -0600

    Update scheduled-deploy.js

--- netlify/functions/scheduled-deploy.js
+++ netlify/functions/scheduled-deploy.js
@@ -4,11 +4,8 @@ import { schedule } from '@netlify/functions'
 const BUILD_HOOK = process.env.BUILD_HOOK_URL
 
 const handler = schedule('0 * * * *', async () => {
-  // Convert current UTC hour to MDT (UTC-6)
-  const currentHourMDT = (new Date().getUTCHours() - 6 + 24) % 24;
 
-  // Check if current MDT hour is between 8 AM and 8 PM
-  if ((currentHourMDT >= 8 && currentHourMDT <= 20) && (BUILD_HOOK)) {
+  if (BUILD_HOOK) {
     await fetch(BUILD_HOOK, {
       method: 'POST'
     }).then(response => {
