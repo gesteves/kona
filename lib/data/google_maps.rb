c13675d3d0d3c55d7faf2359a3fe00ae3193f7c0
Guillermo Esteves <g@gesteves.com>
2023-08-23 07:33:04 -0600

    Remove unused code

--- lib/data/google_maps.rb
+++ lib/data/google_maps.rb
@@ -17,22 +17,6 @@ class GoogleMaps
     @longitude = longitude
   end
 
-  def location
-    data = geocode
-    return if data.blank?
-
-    components = data['results'][0]['address_components']
-    city = components.find { |component| component['types'].include?('locality') }['long_name']
-    region = components.find { |component| component['types'].include?('administrative_area_level_1') }['long_name']
-    country = components.find { |component| component['types'].include?('country') }['long_name']
-
-    if country == 'United States' || country == 'Canada'
-      "#{city}, #{region}"
-    else
-      "#{city}, #{country}"
-    end
-  end
-
   def time_zone
     data = time_zone_data
     return if data.blank?
