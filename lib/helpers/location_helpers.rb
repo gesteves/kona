31a24ff3319d565136bc98aadd63847cdfaa22a8
Guillermo Esteves <g@gesteves.com>
2023-08-24 07:58:09 -0600

    Safe navigation

--- lib/helpers/location_helpers.rb
+++ lib/helpers/location_helpers.rb
@@ -24,7 +24,7 @@ module LocationHelpers
         return [city || county, state].compact.join(", ")
       end
     else
-      if city.downcase =~ /#{country.downcase}/
+      if city&.downcase =~ /#{country&.downcase}/
         return city
       else
         return [city || state, country].compact.join(", ")
