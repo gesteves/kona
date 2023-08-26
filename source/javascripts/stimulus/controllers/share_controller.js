5ff3b63fb697c7ba029eb96dc52c7d76f0797c0f
Guillermo Esteves <g@gesteves.com>
2023-08-26 07:42:13 -0600

    Open share in popup

--- source/javascripts/stimulus/controllers/share_controller.js
+++ source/javascripts/stimulus/controllers/share_controller.js
@@ -1,10 +1,15 @@
 import { Controller } from "stimulus";
 
 export default class extends Controller {
-  static classes = ["hidden"]
+  static classes = ["hidden"];
+  static values = {
+    popupWidth: Number,
+    popupHeight: Number,
+    isNative: Boolean
+  };
 
   connect() {
-    if (navigator.share) {
+    if (navigator.share && this.isNativeValue) {
       this.element.classList.remove(this.hiddenClass);
     }
   }
@@ -14,7 +19,8 @@ export default class extends Controller {
     return canonicalLink ? canonicalLink.href : window.location.href;
   }
 
-  share() {
+  openShareSheet(event) {
+    event.preventDefault();
     const ogTitle = document.querySelector('meta[property="og:title"]')?.content || document.title;
     const url = this.getCanonicalOrFallbackUrl();
     const modifiedUrl = new URL(url);
@@ -26,5 +32,15 @@ export default class extends Controller {
     }).catch(() => {
       // Handle potential error silently
     });
+  }
+
+  openPopup(event) {
+    event.preventDefault();
+    const linkURL = this.element.href;
+    
+    const width = this.popupWidthValue || 400;
+    const height = this.popupHeightValue || 300;
+
+    window.open(linkURL, 'popupWindow', `width=${width},height=${height},scrollbars=yes`);
   }
 }
