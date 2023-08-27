d9a8526c810287c5b09a90a9d9d428f03e195c36
Guillermo Esteves <g@gesteves.com>
2023-08-27 08:18:10 -0600

    Live update content

---
+++ source/javascripts/stimulus/controllers/live_update_controller.js
@@ -0,0 +1,36 @@
+import { Controller } from "@hotwired/stimulus";
+
+export default class extends Controller {
+  static values = {
+    url: String,
+    pollingFrequency: Number
+  };
+
+  connect() {
+    if (this.pollingFrequencyValue > 0) {
+      this.interval = setInterval(() => {
+        this.fetchAndUpdateContent();
+      }, this.pollingFrequencyValue * 1000);
+    }
+  }
+
+  disconnect() {
+    clearInterval(this.interval);
+  }
+
+  async fetchAndUpdateContent() {
+    if (this.urlValue) {
+      try {
+        let response = await fetch(this.urlValue);
+        let data = await response.text();
+        
+        const newElement = document.createElement('div');
+        newElement.innerHTML = data.trim();
+
+        this.element.replaceWith(newElement.firstChild);
+      } catch (error) {
+        console.error('Error fetching content:', error);
+      }
+    }
+  }
+}
