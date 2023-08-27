57c42ccd59a3e1151ec7867d117e89b2abe027f5
Guillermo Esteves <g@gesteves.com>
2023-08-27 14:25:00 -0600

    Fetch update when visibility changes

--- source/javascripts/stimulus/controllers/live_update_controller.js
+++ source/javascripts/stimulus/controllers/live_update_controller.js
@@ -2,35 +2,28 @@ import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
   static values = {
-    url: String,
-    pollingFrequency: Number
+    url: String
   };
 
-  connect() {
-    if (this.hasUrlValue && this.hasPollingFrequencyValue && (this.pollingFrequencyValue >= 60)) {
-      this.interval = setInterval(() => {
-        if (document.visibilityState === "visible") {
-          this.fetchAndUpdateContent();
-        }
-      }, this.pollingFrequencyValue * 1000);
+  handleVisibilityChange() {
+    if (document.visibilityState === "visible") {
+      this.fetchAndUpdateContent();
     }
   }
 
-  disconnect() {
-    clearInterval(this.interval);
-  }
-
   async fetchAndUpdateContent() {
-    try {
-      let response = await fetch(this.urlValue);
-      let data = await response.text();
-      
-      const newElement = document.createElement('div');
-      newElement.innerHTML = data.trim();
-
-      this.element.replaceWith(newElement.firstChild);
-    } catch (error) {
-      console.error('Error fetching content:', error);
+    if (this.hasUrlValue) {
+      try {
+        let response = await fetch(this.urlValue);
+        let data = await response.text();
+        
+        const newElement = document.createElement('div');
+        newElement.innerHTML = data.trim();
+  
+        this.element.replaceWith(newElement.firstChild);
+      } catch (error) {
+        console.error('Error fetching content:', error);
+      }
     }
   }
 }
