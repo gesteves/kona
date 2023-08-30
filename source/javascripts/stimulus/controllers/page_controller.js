a5e0b1370106e754f24c39707bf4c20c6829cb4d
Guillermo Esteves <g@gesteves.com>
2023-08-29 18:33:17 -0600

    Move function

--- source/javascripts/stimulus/controllers/page_controller.js
+++ source/javascripts/stimulus/controllers/page_controller.js
@@ -2,15 +2,7 @@ import { trackPageView } from '../lib/analytics';
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-  cleanUpUrl() {
-    if (window.location.search) {
-      const cleanURL = window.location.origin + window.location.pathname;
-      window.history.replaceState({}, document.title, cleanURL);
-    }
-  }
-
-  handlePageLoad() {
+  load() {
     trackPageView();
-    this.cleanUpUrl();
   }
 }
--- source/javascripts/stimulus/lib/analytics.js
+++ source/javascripts/stimulus/lib/analytics.js
@@ -13,6 +13,7 @@ function setupPlausibleQueue() {
 export function trackPageView() {
   setupPlausibleQueue();
   plausible('pageview');
+  cleanUpUrl();
 }
 
 /**
@@ -22,4 +23,14 @@ export function trackPageView() {
 export function trackEvent(event, props = {}) {
   setupPlausibleQueue();
   plausible(event, { props: props });
+}
+
+/**
+ * Removes UTM params and other crap from the page URL
+ */
+export function cleanUpUrl() {
+  if (window.location.search) {
+    const cleanURL = window.location.origin + window.location.pathname;
+    window.history.replaceState({}, document.title, cleanURL);
+  }
 }
