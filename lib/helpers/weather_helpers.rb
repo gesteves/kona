736af3c9119798c9385c2265286116fc327cf988
Guillermo Esteves <g@gesteves.com>
2023-09-07 11:45:56 -0600

    Change logic

--- lib/helpers/weather_helpers.rb
+++ lib/helpers/weather_helpers.rb
@@ -159,8 +159,8 @@ module WeatherHelpers
     return if todays_forecast.blank?
     text = []
     text << "#{today_or_tonight}'s forecast #{format_forecasted_condition(todays_forecast.restOfDayForecast.conditionCode).downcase}"
-    text << "with a high of #{format_temperature(todays_forecast.temperatureMax)} and a low of #{format_temperature(todays_forecast.temperatureMin)}" if is_daytime?
-    text << "with a low of #{format_temperature(todays_forecast.temperatureMin)}" if !is_daytime?
+    text << "with a high of #{format_temperature(todays_forecast.temperatureMax)} and a low of #{format_temperature(todays_forecast.temperatureMin)}" unless is_evening?
+    text << "with a low of #{format_temperature(todays_forecast.temperatureMin)}" if is_evening?
     text.join(', ')
   end
 
