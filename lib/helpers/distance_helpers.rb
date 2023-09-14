2b2d272788ff58b3c64fb8d545e98963d6d433fd
Guillermo Esteves <g@gesteves.com>
2023-09-14 08:29:15 -0600

    Refactor

--- lib/helpers/distance_helpers.rb
+++ lib/helpers/distance_helpers.rb
@@ -5,34 +5,50 @@ module DistanceHelpers
     thousand: 'kilometers'
   }
 
-  def format_distance(meters)
-    precision = if meters < 10000
-      2
-    elsif meters < 1000000
-      1
-    else
-      0
+  def format_distance(meters, units: 'si')
+    case units
+    when 'si', 'metric'
+      precision = if meters < 10000
+        2
+      elsif meters < 1000000
+        1
+      else
+        0
+      end
+      number_to_human(meters, units: DISTANCE_UNITS, precision: precision, strip_insignificant_zeros: true, significant: false, delimiter: ',')
+    when 'imperial'
+      miles = meters_to_miles(meters)
+      precision = if miles > 1 && miles < 10
+        2
+      elsif miles < 1000
+        1
+      else
+        0
+      end
+      if miles < 1
+        distance = meters_to_yards(meters)
+        units = { unit: "yards" }
+      else
+        distance = miles
+        units = { unit: "miles" }
+      end
+      number_to_human(distance, units: units, precision: precision, strip_insignificant_zeros: true, significant: false, delimiter: ',')
     end
-    number_to_human(meters, units: DISTANCE_UNITS, precision: precision, strip_insignificant_zeros: true, significant: false, delimiter: ',')
   end
 
-  def format_distance_number(meters)
-    format_distance(meters).split(/\s+/).first
+  def format_distance_number(meters, units: 'si')
+    format_distance(meters, units: units).split(/\s+/).first
   end
 
-  def format_distance_unit(meters)
-    format_distance(meters).split(/\s+/).last
+  def format_distance_unit(meters, units: 'si')
+    format_distance(meters, units: units).split(/\s+/).last
   end
 
   def meters_to_miles(meters)
     miles = meters * 0.000621371
-    precision = if miles < 10
-      2
-    elsif miles < 1000
-      1
-    else
-      0
-    end
-    number_to_human(miles, precision: precision, strip_insignificant_zeros: true, significant: false, delimiter: ',')
+  end
+
+  def meters_to_yards(meters)
+    meters * 1.09361
   end
 end
