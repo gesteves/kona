52058dc56bc8adc89394e13d7f788cc49dcb29b4
Guillermo Esteves <g@gesteves.com>
2024-01-31 11:45:35 -0700

    Rename variable

--- lib/helpers/location_helpers.rb
+++ lib/helpers/location_helpers.rb
@@ -1,38 +1,39 @@
 module LocationHelpers
   # Formats location information based on address components from the Google Maps API.
   # Handles special formatting for some specific locations.
+  # @see https://developers.google.com/maps/documentation/geocoding/requests-geocoding#GeocodingResponses
   # @return [String] The formatted location string.
   def format_location
     components = data.location.results.first.address_components
 
-    # Extract city, state, and country names from the components
+    # Extract city, state/region, and country names from the components
     city = components.find { |component| component['types'].include?('locality') }&.long_name
-    state = components.find { |component| component['types'].include?('administrative_area_level_1') }&.long_name
+    region = components.find { |component| component['types'].include?('administrative_area_level_1') }&.long_name
     county = components.find { |component| component['types'].include?('administrative_area_level_2') }&.long_name
     country = components.find { |component| component['types'].include?('country') }&.long_name
 
     # Replace single quotes with curly single quotes, so places like "Coeur d’Alene" look right
     city&.gsub!("'", "’")
-    state&.gsub!("'", "’")
+    region&.gsub!("'", "’")
     county&.gsub!("'", "’")
     country&.gsub!("'", "’")
 
     case country
     when 'United States', 'United Kingdom'
-      if city == 'New York' && state == 'New York'
+      if city == 'New York' && region == 'New York'
         return 'New York City'
-      elsif state == 'District of Columbia'
+      elsif region == 'District of Columbia'
         return 'Washington, DC'
-      elsif county == 'Teton County' && state == 'Wyoming'
+      elsif county == 'Teton County' && region == 'Wyoming'
         return "Jackson Hole, Wyoming"
       else
-        return [city || county, state].compact.join(", ")
+        return [city || county, region].compact.join(", ")
       end
     else
       if city == 'Ciudad de México'
         return "Mexico City"
       else
-        return [city || state, country].compact.join(", ")
+        return [city || region, country].compact.join(", ")
       end
     end
   end
