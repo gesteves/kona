a1a6e79f1f33018d30096f8fab523b186ac33d84
Guillermo Esteves <g@gesteves.com>
2024-02-01 16:41:22 -0700

    Stub some tests

---
+++ spec/lib/helpers/affiliate_links_helpers_spec.rb
@@ -0,0 +1,35 @@
+require 'spec_helper'
+
+RSpec.describe AffiliateLinksHelpers do
+
+  describe '#has_affiliate_links?' do
+    let(:content_with_affiliate) { double('Content', intro: 'Check out this product', body: '<a href="https://www.amazon.com/example?tag=affiliate-20">Product Link</a>') }
+    let(:content_without_affiliate) { double('Content', intro: 'Just some intro', body: '<a href="https://example.com">Normal Link</a>') }
+
+    it 'returns true for content with affiliate links' do
+      expect(has_affiliate_links?(content_with_affiliate)).to be true
+    end
+
+    it 'returns false for content without affiliate links' do
+      expect(has_affiliate_links?(content_without_affiliate)).to be false
+    end
+  end
+
+  describe '#is_affiliate_link?' do
+    it 'returns true for an Amazon affiliate link' do
+      expect(is_affiliate_link?('https://www.amazon.com/example?tag=affiliate-20')).to be true
+    end
+
+    it 'returns true for an Amazon short link' do
+      expect(is_affiliate_link?('https://amzn.to/abc123')).to be true
+    end
+
+    it 'returns false for a non-affiliate Amazon link' do
+      expect(is_affiliate_link?('https://amazon.com/product')).to be false
+    end
+
+    it 'returns false for non-Amazon links' do
+      expect(is_affiliate_link?('https://example.com')).to be false
+    end
+  end
+end
---
+++ spec/lib/helpers/text_helpers_spec.rb
@@ -0,0 +1,30 @@
+require 'spec_helper'
+
+RSpec.describe TextHelpers do
+  describe "#remove_widows" do
+    it "replaces the space between the last two words with a non-breaking space" do
+      expect(remove_widows("Test sentence here")).to eq("Test sentence&nbsp;here")
+      expect(remove_widows("Test sentence")).to eq("Test&nbsp;sentence")
+      expect(remove_widows("Test")).to eq("Test")
+    end
+
+    it "returns nil for blank text" do
+      expect(remove_widows("")).to be_nil
+    end
+  end
+
+  describe "#comma_join_with_and" do
+    it "joins an array with commas and 'and'" do
+      expect(comma_join_with_and(['apple', 'banana', 'cherry'])).to eq("apple, banana and cherry")
+    end
+  end
+
+  describe "#with_indefinite_article" do
+    it "prefixes word with 'a' or 'an' appropriately" do
+      expect(with_indefinite_article("apple")).to eq("an apple")
+      expect(with_indefinite_article("banana")).to eq("a banana")
+      expect(with_indefinite_article("60-minute workout")).to eq("a 60-minute workout")
+      expect(with_indefinite_article("80-minute workout")).to eq("an 80-minute workout")
+    end
+  end
+end
