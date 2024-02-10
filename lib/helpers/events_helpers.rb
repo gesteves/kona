6e406e0037fdd81b59d9850dc8dce26c65e0fb13
Guillermo Esteves <g@gesteves.com>
2024-02-10 12:43:06 -0700

    Tweak helpers

--- lib/helpers/events_helpers.rb
+++ lib/helpers/events_helpers.rb
@@ -1,16 +1,22 @@
 module EventsHelpers
-  # Determines if a given date string represents the current day in a specific time zone.
-  # @param date [String] The date in RFC 3339 format to be compared with the current day.
-  # @return [Boolean] True if the given date is the current day, otherwise false.
+  # Checks if a given date is today based on a specific time zone.
+  # @param date [String] The date to check, expected in a string format parseable by `Time.parse`.
+  # @return [Boolean] Returns true if the given date is today; false otherwise.
   def is_today?(date)
-    event_date = Time.rfc3339(date).in_time_zone(data.time_zone.timeZoneId)
+    event_date = Time.parse(date).in_time_zone(data.time_zone.timeZoneId) # Correction: Use `date` parameter
     today = Time.current.in_time_zone(data.time_zone.timeZoneId)
     event_date.to_date == today.to_date
   end
 
-  # Checks if any of the races scheduled is today and not canceled.
-  # @return [Boolean] True if today is a race day, otherwise false.
+  # Finds today's race event, if any, excluding any that are canceled.
+  # @return [Event, nil] The race event for today if one exists and is not canceled, nil otherwise.
+  def todays_race
+    data.events.find { |e| is_today?(e.date) && !e.canceled }
+  end
+
+  # Determines if today is a race day.
+  # @return [Boolean] True if there is a race event today that is not canceled; false otherwise.
   def is_race_day?
-    data.events.any? { |e| is_today?(e.date) && !e.canceled }
+    todays_race.present?
   end
 end
