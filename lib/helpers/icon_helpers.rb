c5023c4950cea401e007ee421c7c94003e2e6e80
Guillermo Esteves <g@gesteves.com>
2024-02-23 20:49:22 -0700

    Simplify method

--- lib/helpers/icon_helpers.rb
+++ lib/helpers/icon_helpers.rb
@@ -5,13 +5,6 @@ module IconHelpers
   # @param icon_id [String] The unique identifier for the icon.
   # @return [String] The SVG content for the icon, or an empty string if not found.
   def icon_svg(family, style, icon_id)
-    icons_data = data.icons
-
-    if icons_data[family] && icons_data[family][style]
-      icon = icons_data[family][style].find { |i| i['id'] == icon_id }
-      return icon['svg'] if icon.present?
-    end
-
-    ''
+    data.icons.dig(family, style)&.find { |i| i.id == icon_id }&.svg
   end
 end
