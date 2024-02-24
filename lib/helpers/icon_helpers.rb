cda26d9411e1d7f64adb4a4632002b21286b4371
Guillermo Esteves <g@gesteves.com>
2024-02-23 20:51:42 -0700

    Update comment

--- lib/helpers/icon_helpers.rb
+++ lib/helpers/icon_helpers.rb
@@ -3,7 +3,7 @@ module IconHelpers
   # @param family [String] The icon's Font Awesome family (e.g., "classic", "solid", "thin").
   # @param style [String] The icon's Font Awesome style within the family (e.g., "brands").
   # @param icon_id [String] The unique identifier for the icon.
-  # @return [String] The SVG content for the icon, or an empty string if not found.
+  # @return [String] The SVG content for the icon.
   def icon_svg(family, style, icon_id)
     data.icons.dig(family, style)&.find { |i| i.id == icon_id }&.svg
   end
