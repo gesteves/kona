b17462cc0ac8d0593c30c8b3fabb2182d950fb87
Guillermo Esteves <g@gesteves.com>
2024-02-25 13:46:52 -0700

    Don't show stale weather data

--- lib/helpers/weather_helpers.rb
+++ lib/helpers/weather_helpers.rb
@@ -8,6 +8,13 @@ module WeatherHelpers
     thousand: 'm'
   }
 
+  # Validates that the available weather data is still valid and not stale.
+  # @return [Boolean] True if the weather data is still current.
+  def weather_data_is_current?
+    return false if current_weather.blank? || todays_forecast.blank?
+    Time.parse(current_weather.as_of) <= 1.hour.from_now
+  end
+
   # Retrieves the current weather conditions.
   # @return [Hash, nil] The current weather conditions data, or nil if not found.
   def current_weather
