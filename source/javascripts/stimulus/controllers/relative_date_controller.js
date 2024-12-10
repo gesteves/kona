b19fccc9262ffe6dc752f954312bb5dcca74e235
Guillermo Esteves <g@gesteves.com>
2024-12-09 17:16:34 -0700

    Set addSuffix in a value

--- source/javascripts/stimulus/controllers/relative_date_controller.js
+++ source/javascripts/stimulus/controllers/relative_date_controller.js
@@ -4,12 +4,16 @@ import { formatDistanceToNow } from "date-fns";
 export default class extends Controller {
   static values = {
     datetime: String,
+    addSuffix: { type: Boolean, default: true }
   };
 
+  /** 
+   * Replaces an absolute timestamp with a relative one.
+   */
   connect() {
     if (this.hasDatetimeValue) {
       const relativeDate = formatDistanceToNow(new Date(this.datetimeValue), {
-        addSuffix: true,
+        addSuffix: this.addSuffixValue,
       });
 
       this.element.textContent = relativeDate;
