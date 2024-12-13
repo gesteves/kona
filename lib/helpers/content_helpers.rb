d15f8668c3a8323e8243d48bfbd5870f9f2a9d0d
Guillermo Esteves <g@gesteves.com>
2024-12-12 17:30:21 -0700

    Add new helpers module

---
+++ lib/helpers/content_helpers.rb
@@ -0,0 +1,86 @@
+module ContentHelpers
+  # Returns the publicly-visible name for an entry type
+  # @param entry [Object] The entry to check.
+  # @return [String] The name of the entry type
+  def entry_type(entry)
+    return if entry.entry_type.blank?
+    case entry.entry_type
+    when 'Short'
+      'Post'
+    else
+      entry.entry_type
+    end
+  end
+
+  # Determines if the article was published today the current timezone.
+  # @param article [Object] The article.
+  # @return [Boolean] If the article was published today.
+  def published_today?(article)
+    article_date = Time.parse(article.published_at).in_time_zone(location_time_zone)
+    article_date.to_date == current_time.to_date
+  end
+
+  # Returns a permalink anchor tag for the article, with the date it was published.
+  # If the article was published today, includes attributes to render the date as a relative timestamp.
+  # @param article [Object] The article.
+  # @return [String] An <a> tag linking to the article, with a relative or absolute date as the text.
+  def article_permalink_timestamp(article)
+    options = {
+      href: article.path
+    }
+    if published_today?(article)
+      options["data-controller"] = "relative-date"
+      options["data-relative-date-datetime-value"] = DateTime.parse(article.published_at).iso8601
+    end
+    content_tag :a, options do
+      DateTime.parse(article.published_at).strftime('%A, %B %-e, %Y')
+    end
+  end
+
+  # Determines whether the content should be hidden from search engines.
+  # @return [Boolean] Returns true if the page should be hidden from search engines.
+  def hide_from_search_engines?
+    return true unless is_production?
+    return false unless defined?(content)
+    return true if content.draft
+    !content.index_in_search_engines
+  end
+
+  # Selects a specified number of articles related to a given article based on shared tags.
+  # @param article [Object] The reference article for finding related articles.
+  # @param count [Integer] (Optional) The number of related articles to return. Default is 4.
+  # @return [Array<Object>] An array of articles related to the given article, up to the specified count.
+  def related_articles(article, count: 4)
+    tags = article.contentful_metadata.tags.map(&:id)
+    data.articles
+      .reject { |a| a.path == article.path } # Reject the article itself
+      .reject { |a| a.draft } # Reject drafts
+      .reject { |a| a.entry_type == 'Short' } # Reject short posts
+      .sort { |a,b| (b.contentful_metadata.tags.map(&:id) & tags).size <=> (a.contentful_metadata.tags.map(&:id) & tags).size } # Fake relevancy sorting by sorting by number of common tags
+      .take(count) # Take the specified number of articles
+  end
+
+  # Generates a JSON-LD schema string for an article, based on the provided content.
+  # @param content [Object] An object containing the article's data.
+  # @see https://developers.google.com/search/docs/appearance/structured-data/article
+  # @return [String] A JSON-LD formatted string representing the article's schema.
+  def article_schema(content)
+    return if content.draft
+    schema = {
+      "@context": "https://schema.org",
+      "@type": "Article",
+      "headline": sanitize(content.title),
+      "datePublished": DateTime.parse(content.published_at).iso8601,
+      "dateModified": DateTime.parse(content.sys.published_at).iso8601,
+      "author": { "@type": "Person", "name": content.author.name, "url": full_url("/author/#{content.author.slug}") }
+    }
+    if content&.cover_image&.url.present?
+      schema["image"] = ["1000x1000", "1600x900", "1600x1200"].map do |s|
+        w, h = s.split('x')
+        params = { w: w, h: h, fit: 'cover' }
+        cdn_image_url(content.cover_image.url, params)
+      end
+    end
+    schema.to_json
+  end
+end
