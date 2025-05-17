0866d3972a748f647464989943fba8b604bc86cb
Guillermo Esteves <g@gesteves.com>
2025-05-17 14:54:13 -0600

    Revert to before-cache

--- source/javascripts/stimulus/controllers/nav_controller.js
+++ source/javascripts/stimulus/controllers/nav_controller.js
@@ -14,19 +14,20 @@ export default class extends Controller {
   toggleNav(event) {
     event.preventDefault();
     document.body.classList.toggle(this.openClass);
+    this.updateButtonAttributes();
+  }
+
+  updateButtonAttributes() {
     this.buttonTarget.setAttribute("aria-expanded", document.body.classList.contains(this.openClass));
     this.buttonTarget.setAttribute("aria-label", document.body.classList.contains(this.openClass) ? "Close menu" : "Open menu");
   }
 
   /**
-   * When Turbo loads a page from its cache, sometimes it renders it with the nav open,
-   * since it was cached that way.
-   * So, before rendering a Turbo page, remove the open class from the new body.
-   * @param {Event} event  The turbo:before-render event that contains the new body.
+   * Closes the navigation menu.
+   * @param {Event} event The event that triggered the close.
    */
-  closeNavInNewBody(event) {
-    event.preventDefault();
-    event.detail.newBody.classList.remove(this.openClass);
-    event.detail.resume();
+  closeNav(event) {
+    document.body.classList.remove(this.openClass);
+    this.updateButtonAttributes();
   }
 }
