8b7e0febd5601b8942ae827d9cbac9df9cd377a8
Guillermo Esteves <g@gesteves.com>
2025-07-11 15:23:17 -0600

    Revert check

--- lib/helpers/events_helpers.rb
+++ lib/helpers/events_helpers.rb
@@ -17,7 +17,7 @@ module EventsHelpers
   # Returns four events if the next one is in the next 10 days, otherwise returns three events.
   # @return [Array<Event>] An array of event objects that are today or in the future.
   def upcoming_races
-    upcoming = data.events.sort_by { |e| Time.parse(e.date) }.select { |e| Time.parse(e.date).in_time_zone(location_time_zone).beginning_of_day >= current_time.beginning_of_day }
+    upcoming = data.events.sort_by { |e| Time.parse(e.date) }.select { |e| Time.parse(e.date).in_time_zone(location_time_zone).beginning_of_day >= current_time.beginning_of_day && e.going }
     upcoming.take(is_close?(upcoming.first) ? 4 : 3)
   end
 
