8b6fb061111037021e8c8383a182571b50f84ec9
Guillermo Esteves <g@gesteves.com>
2025-11-22 17:53:43 -0700

    Fix tests

--- lib/helpers/affiliate_links_helpers.rb
+++ lib/helpers/affiliate_links_helpers.rb
@@ -1,11 +1,13 @@
 require 'nokogiri'
+require 'public_suffix'
+require 'cgi'
 
 module AffiliateLinksHelpers
   # Checks if the provided content contains any Amazon Associates links.
   # @param content [Object] The content object which may contain affiliate links in its intro or body.
   # @return [Boolean] True if affiliate links are found, otherwise false.
   def has_amazon_associates_links?(content)
-    text = [content.intro, content.body].compact.join("/n/n")
+    text = [content.intro, content.body].compact.join("\n\n")
     doc = Nokogiri::HTML::DocumentFragment.parse(markdown_to_html(text))
     doc.css('a').each do |a|
       return true if is_amazon_associates_link?(a['href'])
@@ -21,7 +23,7 @@ module AffiliateLinksHelpers
       uri = URI.parse(url)
       params = uri.query ? CGI.parse(uri.query) : {}
       domain = PublicSuffix.domain(uri.host)
-      domain == 'amzn.to' || domain == 'amazon.com' && params.include?('tag')
+      domain == 'amzn.to' || (domain == 'amazon.com' && params.key?('tag'))
     rescue
       false
     end
