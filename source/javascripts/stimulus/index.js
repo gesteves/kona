5471dbafe718edc36c57ad55055646cbaadd0140
Guillermo Esteves <g@gesteves.com>
2026-02-21 13:33:02 -0700

    Switch to esbuild

--- source/javascripts/stimulus/index.js
+++ source/javascripts/stimulus/index.js
@@ -1,11 +1,32 @@
 import * as Turbo from '@hotwired/turbo';
 import { Application } from '@hotwired/stimulus';
-import { definitionsFromContext } from '@hotwired/stimulus-webpack-helpers';
 import Handlebars from 'handlebars';
 
+import BackToTopController from './controllers/back_to_top_controller';
+import ClipboardController from './controllers/clipboard_controller';
+import ImagePlaceholderController from './controllers/image_placeholder_controller';
+import LiveUpdateController from './controllers/live_update_controller';
+import NavController from './controllers/nav_controller';
+import NotificationsController from './controllers/notifications_controller';
+import OauthCallbackController from './controllers/oauth_callback_controller';
+import PageController from './controllers/page_controller';
+import RelativeDateController from './controllers/relative_date_controller';
+import ShareController from './controllers/share_controller';
+import UnitsController from './controllers/units_controller';
+
 window.Stimulus = Application.start();
-const context = require.context('./controllers', true, /\.js$/);
-Stimulus.load(definitionsFromContext(context));
+Stimulus.register('back-to-top', BackToTopController);
+Stimulus.register('clipboard', ClipboardController);
+Stimulus.register('image-placeholder', ImagePlaceholderController);
+Stimulus.register('live-update', LiveUpdateController);
+Stimulus.register('nav', NavController);
+Stimulus.register('notifications', NotificationsController);
+Stimulus.register('oauth-callback', OauthCallbackController);
+Stimulus.register('page', PageController);
+Stimulus.register('relative-date', RelativeDateController);
+Stimulus.register('share', ShareController);
+Stimulus.register('units', UnitsController);
+
 Handlebars.registerHelper('pluralize', function (count, singular, plural) {
   return count === 1 ? singular : plural;
 });
